c5a1a0e059a19cd13f2071aaf0bef3c93efb9d9a
Pavel Grigorenko <grigorenkopv@ya.ru>
2022-06-10 15:31:18 +0300

    Login script fix

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ from telethon import TelegramClient
 import config
 
 TelegramClient(
-    session=config.session_name,
+    session="Goodmorning Userbot Session",
     api_id=config.api_id,
     api_hash=config.api_hash,
 ).start()
